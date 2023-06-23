b98a574c6ddc45a285e25c4a95e8bf2b69e8819d
Ola Oredola <oredolaolamide@gmail.com>
2023-06-23 16:56:29 +0100

    basic server

---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+
+import app from './server';
+
+const port = process.env.PORT || 3322;
+
+app.listen(port, () => {
+  console.log(`server listening on ${port}`)
+})
